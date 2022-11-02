552130182830dba2afbf8ce41da693c5eacde751
Valentina Macek <vm5180@student.uni-lj.si>
2022-11-03 00:36:30 +0100

    pobbrane vse strani z leti za bergamo

--- poberi_podatke.py
+++ poberi_podatke.py
@@ -5,6 +5,7 @@ import sys
 import re
 
 ST_STRANI = 109
+PARI = [('11', '2022'),('12', '2022'), ('01', '2023'), ('02', '2023'), ('03', '2023')]
 vzorec_bloka = re.compile(
     r'<div class="table-responsive">.*?<th>FLIGHT</th>.*?<tbody>(.*?)</tbody>',
     flags=re.DOTALL
@@ -23,6 +24,14 @@ vzorec_leta = re.compile(
 vzorec_dest = re.compile(
     r'<a href="/en/seasonal-flights-timetable/calendar.*?/dep/(?P<dest>.*?)/" class',
     flags=re.DOTALL
+)
+
+bvzorec_leta = re.compile(
+    r'<h4 class="modal-title".*?In partenza per (?P<destinacija>.*?) - (?P<datum>.*?)</h4>.*?'
+    r'<th></th></tr><tr>.*?<td>(?P<druzba>.*?)</td><td>(?P<letalo>.*?)</td>.*?'
+    r'<td>(?P<odhod>.*?)</td>.*?'
+    r'<td>(?P<prihod>.*?)</td>',
+     flags=re.DOTALL
 )
 def pripravi_imenik(ime_datoteke):
     '''Če še ne obstaja, pripravi prazen imenik za dano datoteko.'''
@@ -47,36 +56,6 @@ def shrani_spletno_stran(url, ime_datoteke, vsili_prenos=False):
         with open(ime_datoteke, 'w', encoding='utf-8') as datoteka:
             datoteka.write(r.text)
 
-def vsebina_datoteke(ime_datoteke):
-    '''Vrne niz z vsebino datoteke z danim imenom.'''
-    with open(ime_datoteke, encoding='utf-8') as datoteka:
-        return datoteka.read()
-
-def shrani_venezia():
-    stran = 1
-    while stran<110:
-        url = f'https://www.veneziaairport.it/en/flights/seasonal-schedule/fdatefrom-30-10-2022/fdateto-25-03-2023/ftype-D/ftframe-alldaylong/page-{stran}.html'
-        datoteka = f'venezia/{stran}.html'
-        shrani_spletno_stran(url, datoteka)
-        stran+=1    
-shrani_venezia()
-def shrani_bergamo():
-    url = 'https://www.milanbergamoairport.it/en/seasonal-flights-timetable/'
-    datoteka = 'bergamo/frontpage.html'
-    shrani_spletno_stran(url, datoteka)
-shrani_bergamo()
-def shrani_destinacije_bergamo():
-    dat = 'bergamo/frontpage.html'
-    prva_stran_vsebina = vsebina_datoteke(dat)
-    destinacije = []
-    for dest in vzorec_dest.finditer(prva_stran_vsebina):
-        destinacije.append(dest.group(1))
-    for dest_id in destinacije:
-        dat = f'bergamo/{dest_id}.html'
-        url = f'https://www.milanbergamoairport.it/en/seasonal-flights-timetable/calendar/linea/dep/{dest_id}/?m=11&y=2022'
-        shrani_spletno_stran(url, dat)
-
-shrani_destinacije_bergamo()
 
 def download_url_to_string(url):
     """Funkcija kot argument sprejme niz in poskusi vrniti vsebino te spletne
@@ -93,6 +72,47 @@ def download_url_to_string(url):
     # nadaljujemo s kodo če ni prišlo do napake
     return page_content.text
 
+def vsebina_datoteke(ime_datoteke):
+    '''Vrne niz z vsebino datoteke z danim imenom.'''
+    with open(ime_datoteke, encoding='utf-8') as datoteka:
+        return datoteka.read()
+
+def shrani_venezia():
+    stran = 1
+    while True:
+        url = f'https://www.veneziaairport.it/en/flights/seasonal-schedule/fdatefrom-30-10-2022/fdateto-25-03-2023/ftype-D/ftframe-alldaylong/page-{stran}.html'
+        besedilo = download_url_to_string(url)
+        if najdi_lete(besedilo) == None:
+            return None
+        datoteka = f'venezia/{stran}.html'
+        shrani_spletno_stran(url, datoteka)
+        stran+=1    
+
+def shrani_bergamo():
+    url = 'https://www.milanbergamoairport.it/en/seasonal-flights-timetable/'
+    datoteka = 'bergamo/frontpage.html'
+    shrani_spletno_stran(url, datoteka)
+
+def shrani_destinacije_bergamo():
+    dat = 'bergamo/frontpage.html'
+    prva_stran_vsebina = vsebina_datoteke(dat)
+    destinacije = []
+    for dest in vzorec_dest.finditer(prva_stran_vsebina):
+        destinacije.append(dest.group(1))
+    leti= []
+    for dest_id in destinacije:
+        for par in PARI: 
+            url = f'https://www.milanbergamoairport.it/en/seasonal-flights-timetable/calendar/linea/dep/{dest_id}/?m={par[0]}&y={par[1]}'
+            dat = f'bergamo/{dest_id}/{par[0]}.html'
+            shrani_spletno_stran(url, dat)
+            vsebina = vsebina_datoteke(dat)
+            for let_najdba in bvzorec_leta.finditer(vsebina):
+                let = let_najdba.groupdict()
+                leti.append(let)
+    print(leti)
+
+
+
 def najdi_lete(vsebina_strani):
     seznam_letov = vzorec_bloka.findall(vsebina_strani)
     if len(seznam_letov)==1:
@@ -100,14 +120,66 @@ def najdi_lete(vsebina_strani):
        for let_podatki in vzorec_leta.finditer(seznam_letov[0]):
             let = let_podatki.groupdict()
             leti.append(let)
-    return leti
-           
-leti = []   
-for i in range(1, ST_STRANI + 1):
-    dat = f'venezia/{i}.html'
-    vsebina = vsebina_datoteke(dat)
-    letala = najdi_lete(vsebina)
-    leti.extend(letala)
-# print(leti)
-# print(len(leti))
+       return leti
+    else:
+        return None
 
+def vsi_leti_venezia():
+    leti = []   
+    for i in range(1, ST_STRANI + 1):
+        dat = f'venezia/{i}.html'
+        vsebina = vsebina_datoteke(dat)
+        letala = najdi_lete(vsebina)
+        leti.extend(letala)
+    print(leti)
+
+def ustvari_slovar_datumov():
+    meseci = [11, 12, 1, 2, 3]
+    datumi=[]
+    dnevi = ['TOR', 'SRE', 'CET', 'PET', 'SOB', 'NED', 'PON']
+    k = 0
+    for mesec in meseci:
+        if mesec == 11 or mesec == 12:
+            leto = 2022
+            if mesec == 11:
+                for dan in range(1, 31):
+                    datumi.append((dan, mesec, leto, dnevi[k]))
+                    if k != 6:
+                        k+=1
+                    else:
+                        k = 0
+            else:
+                for dan in range(1,32):
+                    datumi.append((dan, mesec, leto,  dnevi[k]))
+                    if k != 6:
+                        k+=1
+                    else:
+                        k = 0
+        else:
+            nleto = 2023
+            if mesec == 2:
+                for dan in range(1, 29):
+                    datumi.append((dan, mesec, nleto,  dnevi[k]))
+                    if k != 6:
+                        k+=1
+                    else:
+                        k = 0
+            else:
+                for dan in range(1,32):
+                    datumi.append((dan, mesec, nleto,  dnevi[k]))
+                    if k != 6:
+                        k+=1
+                    else:
+                        k = 0
+    return datumi
+
+
+
+
+
+
+def main(redownload=True, reparse=True):
+    shrani_venezia()
+
+def vsi_leti_bergamo():
+    leti= []
